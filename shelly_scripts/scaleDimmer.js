62bff017448c525197ba8c218c818b33786c520b
Tom Fili <tom@cesium.com>
2024-02-22 17:40:26 -0500

    Removed proximity config and adjusted new integration to use feet

--- shelly_scripts/scaleDimmer.js
+++ shelly_scripts/scaleDimmer.js
@@ -2,24 +2,25 @@ const KEY = "dimmerState";
 const DEBUG = false;
 
 function updateKVP(on, brightness, oldValues) {
-  if (typeof oldValues === "undefined" || oldValues.on !== on || oldValues.brightness !== brightness) {
+  if (
+    typeof oldValues === "undefined" ||
+    oldValues.on !== on ||
+    oldValues.brightness !== brightness
+  ) {
     const kvp = {
       key: KEY,
       value: JSON.stringify({
         on: on,
-        brightness: brightness
-      })
+        brightness: brightness,
+      }),
     };
-    
+
     if (DEBUG) {
       print("KVS.Set");
       print(JSON.stringify(kvp));
     }
-    
-    Shelly.call(
-      "KVS.Set",
-      kvp
-    );
+
+    Shelly.call("KVS.Set", kvp);
   }
 }
 
@@ -28,31 +29,35 @@ function updateLight(event) {
   if (delta.source === "loopback" || typeof delta.brightness === "undefined") {
     return;
   }
-  
+
   Shelly.call(
     "KVS.Get",
-    {key: KEY},
+    { key: KEY },
     function (entry) {
       const kvp = JSON.parse(entry.value);
-      
+
       if (DEBUG) {
         print("KVS.Get");
         print(JSON.stringify(kvp));
       }
-      
+
       let newBrightness = delta.brightness;
       const newIsOn = delta.output;
       if (newBrightness !== kvp.brightness) {
         newBrightness = Math.floor(newBrightness * 0.75);
-        Shelly.call("Light.Set",
-          {"id":event.id, "on":newIsOn, "brightness":newBrightness},
-          null);
+        Shelly.call(
+          "Light.Set",
+          { id: event.id, on: newIsOn, brightness: newBrightness },
+          null
+        );
       }
       updateKVP(newIsOn, newBrightness, kvp);
-    }, null);
+    },
+    null
+  );
 }
 
-const lightStatus = Shelly.getComponentStatus("light",0);
+const lightStatus = Shelly.getComponentStatus("light", 0);
 updateKVP(lightStatus.output, lightStatus.brightness);
 
 Shelly.addStatusHandler(updateLight, null);
